99eae18c1e4536b584e37aea093a5a318bc92e73
zG <22377978+zenGator@users.noreply.github.com>
2019-03-21 17:50:31 -0400

    Create mw2xwf.pl

---
+++ mw2xwf.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# https://github.com/zenGator/mw2xwf
+# zG:20190321
+
+use strict;
+use warnings;
+
+my $infile="$ARGV[0]";
+open(my $fh, '<:encoding(UTF-8)', $infile)
+  or die "Could not open file '$infile' $!";
+my $x=0;
+while (my $row = <$fh>) {  
+#get a line
+  $x++;
+#  printf "$x: ";
+  chomp $row;
+  my $orig_row=$row;
+#  printf ("%-60s => \n", $row);
+  
+# transforms
+  $row =~ s/([^\\]\[[^]]*)\\d([^]]*\])/$1#$2/g;
+  $row =~ s/\\d/#/g;
+  $row =~ s/([^\\]\[[^]]*)\\w([^]]*\])/$1_a-z#$2/g;
+  $row =~ s/\\w/[_a-z#]/g;
+
+  if ( $row =~ /[^\\]\[[^]]*\\b[^\]]*\]/ ) {
+    warn "word boundary inside braces (could be problematic):  ## on line $x\n";
+  }
+#  $row =~ s/(\[[^]]*)\\b([^]]*\])/$1_a-z#$2/g;
+  $row =~ s/\\b/[^_a-z#]/g;
+  $row =~ s/([^\\]\[[^]]*)\\W([^\]]*\])/$1\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E$2/g;
+  $row =~ s/\\W/[\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E]/g;
+  $row =~ s/([^\\]\[[^]]*)\\s([^\]]*\])/$1 \\x09-\\x0D$2/g;
+  $row =~ s/\\s/[ \\x09-\\x0D]/g;
+  $row =~ s/([^\\]\[[^]]*)0-9([^\]]*\])/$1#$2/g;
+  $row =~ s/\{(,[^}]*\})/\{0$1/g;
+
+  
+  if ($row =~ /##/ ) {
+    warn "repetitive repetition of ## on line $x:  recommend manual review\n";
+    warn "\t$orig_row\n\t$row\n";
+  }
+  if ($row =~ /[^\\]\]\[/ ) {
+    warn "sequential char sets on line $x:  recommend manual review\n";
+    warn "\t$orig_row\n\t$row\n";
+  }
+#show results
+#  printf ("   %-60s \n\n", $row);
+  printf ("%s\n", $row);
+# brevity
+#  if ($x > 10) {die;}
+}
